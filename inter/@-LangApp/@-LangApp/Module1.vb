d9171d279b073c2033d491d96e6b64a3b03f0722
giantpanda9 <61800770+giantpanda9@users.noreply.github.com>
2020-12-17 13:36:41 +0300

    Add files via upload

---
+++ inter/@-LangApp/@-LangApp/Module1.vb
@@ -0,0 +1,15 @@
+Module Module1
+    Dim value As String
+    Sub Main()
+
+
+        value = "Hello World"
+
+        Console.Title = value
+
+        Console.WriteLine("Hello World")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
